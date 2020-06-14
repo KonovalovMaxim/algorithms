d8db9c2f9b87a264565fd1f56eea9a934ea323e6
Konovalov Maxim <maxim.kon@skyeng.ru>
2020-06-14 14:48:17 +0900

    new-year-chaos.py

--- hackerrank/interview-preparation-kit/arrays/new-year-chaos.py
+++ hackerrank/interview-preparation-kit/arrays/new-year-chaos.py
@@ -10,42 +10,39 @@ import sys
 def minimumBribes(q):
     counter = 0
     map = [0] * len(q)
-    currentIndex = 0
-    while currentIndex < (len(q) - 1):
-        if q[currentIndex + 1] - q[currentIndex] == 1:
-            currentIndex += 1
+    i = len(q) - 1
+    searchFor = len(q)
+    while i > 0:
+        if q[i] == searchFor:
+            i -= 1
+            searchFor -= 1
             continue
-        if (currentIndex == 0 and q[currentIndex] != 1):
-            next = 1
-        else:
-            next = q[currentIndex] + 1
-
-        j = currentIndex
-
-        while q[j] != next:
-            j += 1
-        if q[j - 1] > q[j]:
-            tmp = q[j]
-            q[j] = q[j - 1]
-            q[j - 1] = tmp
-            map[q[j] - 1] += 1
-            counter += 1
-            if map[q[j] - 1] > 2 or map[q[j - 1] - 1] > 2:
-                return "Too chaotic"
-
+        j = (i - 1)
+        while q[j] != searchFor:
+            j -= 1
+        tmp = q[j + 1]
+        q[j + 1] = q[j]
+        q[j] = tmp
+        counter += 1
+        map[searchFor - 1] += 1
+        if map[searchFor - 1] > 2:
+            return "Too chaotic"
     return counter
 
 
-if __name__ == '__main__':
-    file = open('./new-year-chaos/input06.txt')
-    t = int(file.readline())
-    out = ""
-    for t_itr in range(t):
-        n = int(file.readline())
+# print(minimumBribes([1, 2, 5, 3, 4, 7, 8, 6]))
+print(minimumBribes([2, 5, 1, 3, 4]))
 
-        q = list(map(int, file.readline().rstrip().split()))
-
-        out += str(minimumBribes(q)) + "\n"
-
-expected = open("./new-year-chaos/output06.txt").read()
-assert out.strip() == expected
+# if __name__ == '__main__':
+#     file = open('./new-year-chaos/input03.txt')
+#     t = int(file.readline())
+#     out = ""
+#     for t_itr in range(t):
+#         n = int(file.readline())
+#
+#         q = list(map(int, file.readline().rstrip().split()))
+#
+#         out += str(minimumBribes(q)) + "\n"
+#
+# expected = open("./new-year-chaos/output03.txt").read()
+# assert out.strip() == expected
