a09545efbdd2bf3bc94dac60aa1c1a4b2404270e
Konovalov Maxim <maxim.kon@skyeng.ru>
2020-07-15 04:21:38 +0900

    (leetcode) 709. To Lower Case https://leetcode.com/problems/to-lower-case/

---
+++ leetcode/leetcode/editor/en/[709]To Lower Case.php
@@ -0,0 +1,49 @@
+<?php
+//Implement function ToLowerCase() that has a string parameter str, and returns
+//the same string in lowercase. 
+//
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: "Hello"
+//Output: "hello"
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: "here"
+//Output: "here"
+// 
+//
+// 
+// Example 3: 
+//
+// 
+//Input: "LOVELY"
+//Output: "lovely"
+// 
+// 
+// 
+// Related Topics String 
+// 👍 481 👎 1537
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution
+{
+
+    /**
+     * @param String $str
+     * @return String
+     */
+    function toLowerCase($str)
+    {
+        return strtolower($str);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
