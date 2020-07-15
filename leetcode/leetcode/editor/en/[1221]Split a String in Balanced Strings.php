f7aecf9d91909fd29a8888fdd685c5795e8ef0b5
Konovalov Maxim <maxim.kon@skyeng.ru>
2020-07-15 15:26:38 +0900

    (leetcode) 1221. Split a String in Balanced Strings https://leetcode.com/problems/split-a-string-in-balanced-strings/

---
+++ leetcode/leetcode/editor/en/[1221]Split a String in Balanced Strings.php
@@ -0,0 +1,81 @@
+<?php
+//Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+//
+//
+// Given a balanced string s split it in the maximum amount of balanced strings.
+// 
+//
+// Return the maximum amount of splitted balanced strings. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "RLRRLLRLRL"
+//Output: 4
+//Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring cont
+//ains same number of 'L' and 'R'.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "RLLLLRRRLR"
+//Output: 3
+//Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains
+// same number of 'L' and 'R'.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "LLLLRRRR"
+//Output: 1
+//Explanation: s can be split into "LLLLRRRR".
+// 
+//
+// Example 4: 
+//
+// 
+//Input: s = "RLRRRLLRLL"
+//Output: 2
+//Explanation: s can be split into "RL", "RRRLLRLL", since each substring contai
+//ns an equal number of 'L' and 'R'
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 1000 
+// s[i] = 'L' or 'R' 
+// 
+// Related Topics String Greedy 
+// 👍 528 👎 359
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function balancedStringSplit($s) {
+        $counter = 0;
+        $runner = 0;
+        $s = str_split($s);
+        for ($i=0;$i<count($s); $i++) {
+            if($s[$i] =='L') {
+                $runner--;
+            } else {
+                $runner++;
+            }
+            if($runner == 0) {
+                $counter++;
+            }
+        }
+        return $counter;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
