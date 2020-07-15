df8a27365c813d65781215aa223dc7f805dff253
Konovalov Maxim <maxim.kon@skyeng.ru>
2020-07-15 15:13:08 +0900

    (leetcode) 1486. XOR Operation in an Array https://leetcode.com/problems/xor-operation-in-an-array/

---
+++ leetcode/leetcode/editor/en/[1486]XOR Operation in an Array.php
@@ -0,0 +1,73 @@
+<?php
+//Given an integer n and an integer start.
+//
+// Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.le
+//ngth. 
+//
+// Return the bitwise XOR of all elements of nums. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: n = 5, start = 0
+//Output: 8
+//Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) 
+//= 8.
+//Where "^" corresponds to bitwise XOR operator.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: n = 4, start = 3
+//Output: 8
+//Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8. 
+//
+// Example 3: 
+//
+// 
+//Input: n = 1, start = 7
+//Output: 7
+// 
+//
+// Example 4: 
+//
+// 
+//Input: n = 10, start = 5
+//Output: 2
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= n <= 1000 
+// 0 <= start <= 1000 
+// n == nums.length 
+// Related Topics Array Bit Manipulation 
+// 👍 121 👎 57
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution
+{
+
+    /**
+     * @param Integer $n
+     * @param Integer $start
+     * @return Integer
+     */
+    function xorOperation($n, $start)
+    {
+        $result = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $result ^= $start + $i * 2;
+        }
+        return $result;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+print((new Solution())->xorOperation(5, 0));
