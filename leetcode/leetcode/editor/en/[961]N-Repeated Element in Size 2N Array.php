1e62acf26113d5c54be989ad684eefa74f29c71e
Konovalov Maxim <maxim.kon@skyeng.ru>
2020-07-19 15:53:47 +0900

    (leetcode) 961. N-Repeated Element in Size 2N Array https://leetcode.com/problems/n-repeated-element-in-size-2n-array/

---
+++ leetcode/leetcode/editor/en/[961]N-Repeated Element in Size 2N Array.php
@@ -0,0 +1,63 @@
+<?php
+//In a array A of size 2N, there are N+1 unique elements, and exactly one of the
+//se elements is repeated N times. 
+//
+// Return the element repeated N times. 
+//
+// 
+//
+// 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: [1,2,3,3]
+//Output: 3
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: [2,1,2,5,3,2]
+//Output: 2
+// 
+//
+// 
+// Example 3: 
+//
+// 
+//Input: [5,1,5,2,5,3,5,4]
+//Output: 5
+// 
+//
+// 
+//
+// Note: 
+//
+// 
+// 4 <= A.length <= 10000 
+// 0 <= A[i] < 10000 
+// A.length is even 
+// 
+// 
+// 
+// 
+// Related Topics Hash Table 
+// 👍 447 👎 225
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * @param Integer[] $A
+     * @return Integer
+     */
+    function repeatedNTimes($A) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
