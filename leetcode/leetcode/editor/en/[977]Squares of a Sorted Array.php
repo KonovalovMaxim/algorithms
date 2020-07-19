424dbe649a855ad88992202e81224240e61c0d00
Konovalov Maxim <maxim.kon@skyeng.ru>
2020-07-19 16:00:59 +0900

    (leetcode) 977. Squares of a Sorted Array https://leetcode.com/problems/squares-of-a-sorted-array/

---
+++ leetcode/leetcode/editor/en/[977]Squares of a Sorted Array.php
@@ -0,0 +1,48 @@
+<?php
+//Given an array of integers A sorted in non-decreasing order, return an array o
+//f the squares of each number, also in sorted non-decreasing order. 
+//
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: [-7,-3,2,3,11]
+//Output: [4,9,9,49,121]
+// 
+//
+// 
+//
+// Note: 
+//
+// 
+// 1 <= A.length <= 10000 
+// -10000 <= A[i] <= 10000 
+// A is sorted in non-decreasing order. 
+// 
+// 
+// Related Topics Array Two Pointers 
+// 👍 1194 👎 88
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * @param Integer[] $A
+     * @return Integer[]
+     */
+    function sortedSquares($A) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
